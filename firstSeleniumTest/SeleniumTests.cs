c240159d095376305275df6b6dcd0ee2adaf5df9
Dmitry Babintsev <dimababintcev@gmail.com>
2025-04-10 23:39:23 +0500

    Merge branch 'homework'

--- firstSeleniumTest/SeleniumTests.cs
+++ firstSeleniumTest/SeleniumTests.cs
@@ -7,53 +7,39 @@ namespace firstSeleniumTest;
 
 public class Tests
 {
+    private ChromeDriver driver;
     [SetUp]
-    public void Setup()
+    public void SetUp()
     {
+        driver = new();
+        Authorize("", "");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Quit();
     }
 
     [Test]
     public void CheckAuthorizationTest()
     {
-        var driver = new ChromeDriver();
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
-
-        var login = driver.FindElement(By.Id("Username"));
-        login.SendKeys("");
-
-        var password = driver.FindElement(By.Id("Password"));
-        password.SendKeys("");
-
-        var enter = driver.FindElement(By.Name("button"));
-        enter.Click();
-
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-        wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[data-tid='Title']")));
+        var wait = DoExplicitWait();
 
         Assert.That(driver.Title, Does.Contain("Новости"));
+    }
 
-        driver.Quit();
+    private WebDriverWait DoExplicitWait()
+    {
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+        wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[data-tid='Title']")));
+        return wait;
     }
 
     [Test]
     public void CheckNavigationTest()
     {
-        var driver = new ChromeDriver();
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
-
-        var login = driver.FindElement(By.Id("Username"));
-        login.SendKeys("");
-
-        var password = driver.FindElement(By.Id("Password"));
-        password.SendKeys("");
-
-        var enter = driver.FindElement(By.Name("button"));
-        enter.Click();
-
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-        wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[data-tid='Title']")));
+        var wait = DoExplicitWait();
 
         var enterCommunities = driver.FindElement(By.CssSelector("[data-tid='Community']"));
         enterCommunities.Click();
@@ -61,6 +47,20 @@ public class Tests
         wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[data-tid='Title']")));
 
         Assert.That(driver.Title, Does.Contain("Сообщества"));
-        driver.Quit();
+    }
+
+    private void Authorize(string userLogin, string userPassword)
+    {
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
+
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys(userLogin);
+
+        var password = driver.FindElement(By.Id("Password"));
+        password.SendKeys(userPassword);
+
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
     }
 }
