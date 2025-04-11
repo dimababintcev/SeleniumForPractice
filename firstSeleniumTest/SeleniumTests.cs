f3b57d0e4d28a8d75ebaf41ef6e0a1454d556e21
Dmitry Babintsev <dimababintcev@gmail.com>
2025-04-11 05:09:18 +0500

    add CreateCommunityTest

--- firstSeleniumTest/SeleniumTests.cs
+++ firstSeleniumTest/SeleniumTests.cs
@@ -2,17 +2,26 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
+using FluentAssertions;
 
 namespace firstSeleniumTest;
 
+[Parallelizable(ParallelScope.All)]
 public class Tests
 {
-    private ChromeDriver driver;
+    private readonly ChromeDriver driver;
+    private readonly WebDriverWait wait;
+
+    public Tests()
+    {
+        driver = new();
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(100));
+    }
+
     [SetUp]
     public void SetUp()
     {
-        driver = new();
-        Authorize("", "");
+        Authorize("dimababintcev@gmail.com", "65spWCM73gfr.");
     }
 
     [TearDown]
@@ -24,29 +33,58 @@ public class Tests
     [Test]
     public void CheckAuthorizationTest()
     {
-        var wait = DoExplicitWait();
-
-        Assert.That(driver.Title, Does.Contain("Новости"));
-    }
-
-    private WebDriverWait DoExplicitWait()
-    {
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-        wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[data-tid='Title']")));
-        return wait;
+        driver.Title.Should().Contain("Новости");
     }
 
     [Test]
-    public void CheckNavigationTest()
+    public void CheckNavigationToCommunitiesTest()
     {
-        var wait = DoExplicitWait();
-
         var enterCommunities = driver.FindElement(By.CssSelector("[data-tid='Community']"));
         enterCommunities.Click();
 
-        wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[data-tid='Title']")));
+        DoExplicitWait();
 
         Assert.That(driver.Title, Does.Contain("Сообщества"));
+    }
+
+    [Test]
+    public void AddCommunityTest()
+    {
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/communities");
+        DoExplicitWait();
+        var addCommunity = driver.FindElement(By.CssSelector("[class='sc-juXuNZ sc-ecQkzk WTxfS vPeNx']"));
+        addCommunity.Click();
+        DoExplicitWait();
+        var communityName = driver.FindElement(By.CssSelector("[placeholder='Название сообщества']"));
+        communityName.Click();
+        communityName.SendKeys("TestCommunity");
+        var create = driver.FindElement(By.CssSelector("[data-tid='CreateButton']"));
+        create.Click();
+        DoExplicitWait();
+        var titleElement = driver.FindElement(By.CssSelector("[data-tid='DeleteButton']"));
+        Assert.That(titleElement.Text, Does.Contain("Удалить сообщество"));
+    }
+
+    //[Test]
+    //public void LeaveCommunityTest()
+    //{
+    //    driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/communities/4eb36d5c-faeb-460e-b055-9f7e09f9d9c5");
+    //    DoExplicitWait();
+    //    var join = driver.FindElement(By.CssSelector("[data-tid=\"Join\"]"));
+    //    join.Click();
+    //    DoExplicitWait("[data-tid=\"PopupMenu__caption\"]");
+    //    var dropDown = driver.FindElement(By.ClassName("sc-kLojOw"));
+    //    dropDown.Click();
+    //    var leave = driver.FindElement(By.CssSelector("[data-tid=\"Quit\"]"));
+    //    leave.Click();
+    //    DoExplicitWait();
+    //    var joinAgain = driver.FindElement(By.CssSelector("[class=\"sc-eWnToP fEDxD\"]"));
+    //    Assert.That(joinAgain, Does.Contain("Вступить"));
+    //}
+
+    private void DoExplicitWait(string locator = "[data-tid='Title']")
+    {
+        wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(locator)));
     }
 
     private void Authorize(string userLogin, string userPassword)
@@ -62,5 +100,7 @@ public class Tests
 
         var enter = driver.FindElement(By.Name("button"));
         enter.Click();
+
+        DoExplicitWait();
     }
 }
